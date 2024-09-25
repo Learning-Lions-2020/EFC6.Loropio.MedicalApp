ccd1a6e93515f3afff5dd77fd9987ee1e4e83372
Jerry Lokoroi <jerrylokoroi@gmail.com>
2024-09-25 16:19:32 +0300

    Changed API solutions name to MedicalRecord.API

---
+++ MedicalApp.Domain/Contracts/IRecordsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Domain.Contracts
+{
+    public interface IRecordsRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
