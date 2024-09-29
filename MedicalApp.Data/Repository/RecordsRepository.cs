5e261dc7178e7734374ddc109b6493423c0af7ad
Jerry Lokoroi <jerrylokoroi@gmail.com>
2024-09-29 11:08:10 +0300

    #Changed API solutions name to MedicalRecord.API

--- MedicalApp.Data/Repository/RecordsRepository.cs
+++ MedicalApp.Data/Repository/RecordsRepository.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MedicalApp.Data.Repository
@@ -21,35 +22,46 @@ namespace MedicalApp.Data.Repository
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _dbSet.ToListAsync(); 
+            return await _dbSet.ToListAsync();
+            return await _dbSet.ToListAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
         {
-            return await _dbSet.FindAsync(id); 
+            return await _dbSet.FindAsync(id);
+            return await _dbSet.FindAsync(id);
         }
 
         public async Task AddAsync(T entity)
         {
-            await _dbSet.AddAsync(entity); 
-            await _context.SaveChangesAsync(); 
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(T entity)
         {
-            _dbSet.Update(entity); 
-            await _context.SaveChangesAsync(); 
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(int id)
         {
-            var entity = await _dbSet.FindAsync(id); 
+            var entity = await _dbSet.FindAsync(id);
             if (entity != null)
             {
-                _dbSet.Remove(entity); 
-                await _context.SaveChangesAsync();
+                _dbSet.Remove(entity);
+
+                var entity = await _dbSet.FindAsync(id);
+                if (entity != null)
+                {
+                    _dbSet.Remove(entity);
+                    await _context.SaveChangesAsync();
+                }
             }
         }
     }
-
-}
